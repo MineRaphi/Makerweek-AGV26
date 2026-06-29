0aedd1598545ccc730f17acd2202dc514e956c05
MineRaphi <MineRaphiHD@gmail.com>
2026-06-29 11:34:05 +0200

    improved line detection value

--- camera-feed.py
+++ camera-feed.py
@@ -96,8 +96,8 @@ def detect_blue_lines(warped):
     hsv = cv2.cvtColor(warped, cv2.COLOR_BGR2HSV)
 
     # Blue range in HSV
-    lower_blue = np.array([100, 80, 50])
-    upper_blue = np.array([130, 255, 255])
+    lower_blue = np.array([100, 130, 50])
+    upper_blue = np.array([130, 200, 255])
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
 
     # Clean up noise
