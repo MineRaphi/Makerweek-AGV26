2e72424bde63873fd8627fa4466df51743156282
MineRaphi <MineRaphiHD@gmail.com>
2026-06-30 09:37:42 +0200

    added step move

--- drive.py
+++ drive.py
@@ -2,7 +2,9 @@ import requests
 import numpy as np
 
 AGV_BASE_URL = "http://172.17.1.42/api/agv"
-WHEEL_DIAMETER = 95 # 95mm
+# distance in mm
+WHEEL_DIAMETER = 95
+WHEEL_DISTANCE = 210 
 
 def enable_wheels():
     response = requests.post(
@@ -30,3 +32,23 @@ def set_velocity(left, right):
 
 def stop():
     return set_velocity(0.0, 0.0)
+
+def move_steps(left, right):
+    response = requests.post(
+        f"{AGV_BASE_URL}/stepper/setMoveRelative",
+        json={
+            "leftDelta_steps": left,
+            "rightDelta_steps": right
+        }
+    )
+    return response
+
+def set_step(left, right):
+    response = requests.post(
+        f"{AGV_BASE_URL}/stepper/setMoveAbsolute",
+        json={
+            "leftPos_steps": left,
+            "rightPos_steps": right
+        }
+    )
+    return response
